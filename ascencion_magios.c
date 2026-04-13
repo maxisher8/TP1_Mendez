febada5008bf79759ea1deac8ccecfefc100d77b
Cami-loiacono <you@example.com>
2026-04-13 15:03:26 -0300

    primer avance

---
+++ ascencion_magios.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+#include  <stdio.h>
+#include "ascencion_magios.h"
+
+
+void inicializar_juego(juego_t *juego){
+    juego->niveles->paredes
+}
+
+void mostrar_juego(juego_t juego){
+
+}
