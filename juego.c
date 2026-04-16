159337c8efba23c86eb58d97d16a8d62501c7378
Maximiliano sher <116585157+maxisher8@users.noreply.github.com>
2026-04-16 20:30:39 -0300

    actualizacion para que repregunte los movimientos

--- juego.c
+++ juego.c
@@ -6,7 +6,15 @@
 
 int main(){
     juego_t juego;
+    char movimiento = 'q';
     inicializar_juego(&juego);
     mostrar_juego(juego);
+    while(estado_nivel(juego.niveles[juego.nivel_actual], juego.homero) == 0){
+        printf("ingrese un movimiento");
+        scanf(" %c", &movimiento);
+        realizar_jugada(&juego, movimiento);
+        system("clear");
+        mostrar_juego(juego);
+    }
     return 0;
 }
