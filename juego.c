226f93dfaa024d46856153c7fe66344d16c21672
Maximiliano sher <116585157+maxisher8@users.noreply.github.com>
2026-04-26 01:39:38 -0300

    funciones a implementar
    Implement game loop to handle player movements and game state.

--- juego.c
+++ juego.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include "ascension_magios.h"
 
+//funciones a implementar
+
 void pedir_movimiento(char *movimiento){
     printf("Ingrese un movimiento (W/A/S/D para moverse, H para usar hechizo, L para usar antorcha): ");
     scanf(" %c", movimiento);
@@ -32,9 +34,22 @@ void imprimir_resultado(juego_t juego){
 int main(){
     srand ((unsigned)time(NULL)); 
     juego_t juego;
+    char movimiento = 'q';
     inicializar_juego(&juego);
     mostrar_juego(juego);
-    jugar_juego(juego);
-    imprimir_resultado(juego);
+    while(estado_nivel(juego.niveles[juego.nivel_actual - 1], juego.homero) == 0 && estado_juego(juego) == 0){
+        printf("ingrese un movimiento");
+        scanf(" %c", &movimiento);
+        realizar_jugada(&juego, movimiento);
+        mostrar_juego(juego);
+    }
+    if(estado_juego(juego) == -1){
+        printf("¡Has perdido! No te quedan vidas.\n");
+        return 0;
+    }
+    else if(estado_juego(juego) == 1){
+        printf("¡Felicidades! Has ganado el juego.\n");
+        return 0;
+    }
     return 0;
 }
