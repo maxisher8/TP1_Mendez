ceef8c980fa2bf4b022812a18cb097ea380bb095
Maximiliano sher <116585157+maxisher8@users.noreply.github.com>
2026-04-25 23:07:18 -0300

    modularizado

--- juego.c
+++ juego.c
@@ -4,25 +4,36 @@
 #include <stdlib.h>
 #include "ascension_magios.h"
 
-int main(){
-    srand ((unsigned)time(NULL)); 
-    juego_t juego;
+void pedir_movimiento(char *movimiento){
+    printf("Ingrese un movimiento (W/A/S/D para moverse, H para usar hechizo, L para usar antorcha): ");
+    scanf(" %c", movimiento);
+}
+
+void jugar_juego(juego_t juego){
     char movimiento = 'q';
-    inicializar_juego(&juego);
-    mostrar_juego(juego);
     while(estado_nivel(juego.niveles[juego.nivel_actual - 1], juego.homero) == 0 && estado_juego(juego) == 0){
-        printf("ingrese un movimiento");
-        scanf(" %c", &movimiento);
+        pedir_movimiento(&movimiento);
         realizar_jugada(&juego, movimiento);
         mostrar_juego(juego);
     }
-    if(estado_juego(juego) == -1){
+}
+
+void imprimir_resultado(juego_t juego){
+    int estado = estado_juego(juego);
+    if(estado == -1){
         printf("¡Has perdido! No te quedan vidas.\n");
-        return 0;
     }
-    else if(estado_juego(juego) == 1){
+    else if(estado == 1){
         printf("¡Felicidades! Has ganado el juego.\n");
-        return 0;
     }
+}
+
+int main(){
+    srand ((unsigned)time(NULL)); 
+    juego_t juego;
+    inicializar_juego(&juego);
+    mostrar_juego(juego);
+    jugar_juego(juego);
+    imprimir_resultado(juego);
     return 0;
 }
