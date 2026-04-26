99e1382fbdd24b080f7ce018229407d7cb1b02be
Maximiliano sher <116585157+maxisher8@users.noreply.github.com>
2026-04-26 17:39:46 -0300

    Buenas practicas
    Replaced manual movement input loop with jugar_juego function.

--- juego.c
+++ juego.c
@@ -34,22 +34,9 @@ void imprimir_resultado(juego_t juego){
 int main(){
     srand ((unsigned)time(NULL)); 
     juego_t juego;
-    char movimiento = 'q';
     inicializar_juego(&juego);
     mostrar_juego(juego);
-    while(estado_nivel(juego.niveles[juego.nivel_actual - 1], juego.homero) == 0 && estado_juego(juego) == 0){
-        printf("ingrese un movimiento");
-        scanf(" %c", &movimiento);
-        realizar_jugada(&juego, movimiento);
-        mostrar_juego(juego);
-    }
-    if(estado_juego(juego) == -1){
-        printf("¡Has perdido! No te quedan vidas.\n");
-        return 0;
-    }
-    else if(estado_juego(juego) == 1){
-        printf("¡Felicidades! Has ganado el juego.\n");
-        return 0;
-    }
+    jugar_juego(juego);
+    imprimir_resultado(juego);
     return 0;
 }
